af3b4af600925080df5a16a9484c1b467b432c67
Claudiu <claudiu-meiu@proton.me>
2026-08-21 19:09:13 +0300

    Change naming style from Camel case to Snake case

--- pong.c
+++ pong.c
@@ -8,28 +8,28 @@ typedef struct Ball {
 
 int main(void) {
   // INIT
-  const int screenWidth = 1920;
-  const int screenHeight = 1080;
+  const int screen_width = 1920;
+  const int screen_height = 1080;
 
-  InitWindow(screenWidth, screenHeight, "Da Pong");
+  InitWindow(screen_width, screen_height, "Da Pong");
   ToggleFullscreen();
 
-  Rectangle leftPaddle = {300.0f - 10.0f / 2.0f,
-                          GetScreenHeight() / 2.0f - 100.0f / 2.0f, 10.0f,
-                          100.0f};
-
-  Rectangle rightPaddle = {GetScreenWidth() - 300.0f - 10.0f / 2.0f,
+  Rectangle left_paddle = {300.0f - 10.0f / 2.0f,
                            GetScreenHeight() / 2.0f - 100.0f / 2.0f, 10.0f,
                            100.0f};
 
+  Rectangle right_paddle = {GetScreenWidth() - 300.0f - 10.0f / 2.0f,
+                            GetScreenHeight() / 2.0f - 100.0f / 2.0f, 10.0f,
+                            100.0f};
+
   Ball ball = {{GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f}, 10};
 
-  float leftPaddleSpeed = 1500.0f;
-  float rightPaddleSpeed = 1500.0f;
-  Vector2 ballSpeed = {500.0f, 500.0f};
+  float left_paddle_speed = 1500.0f;
+  float right_paddle_speed = 1500.0f;
+  Vector2 ball_speed = {500.0f, 500.0f};
 
-  bool ballCollisionWithLeftPaddle = false;
-  bool ballCollisionWithRightPaddle = false;
+  bool ball_collision_with_left_paddle = false;
+  bool ball_collision_with_right_paddle = false;
 
   SetTargetFPS(0);
 
@@ -39,74 +39,74 @@ int main(void) {
 
     // Left paddle moving up
     if (IsKeyDown(KEY_W)) {
-      if (leftPaddle.y <= 0) {
-        leftPaddleSpeed += 0;
+      if (left_paddle.y <= 0) {
+        left_paddle_speed += 0;
       } else {
-        leftPaddle.y -= GetFrameTime() * leftPaddleSpeed;
+        left_paddle.y -= GetFrameTime() * left_paddle_speed;
       }
     }
     // Left paddle moving down
     if (IsKeyDown(KEY_S)) {
-      if (leftPaddle.y >= (GetScreenHeight() - leftPaddle.height)) {
-        leftPaddleSpeed += 0;
+      if (left_paddle.y >= (GetScreenHeight() - left_paddle.height)) {
+        left_paddle_speed += 0;
       } else {
-        leftPaddle.y += GetFrameTime() * leftPaddleSpeed;
+        left_paddle.y += GetFrameTime() * left_paddle_speed;
       }
     }
     // Right paddle moving up
     if (IsKeyDown(KEY_UP)) {
-      if (rightPaddle.y <= 0) {
-        rightPaddleSpeed += 0;
+      if (right_paddle.y <= 0) {
+        right_paddle_speed += 0;
       } else {
-        rightPaddle.y -= GetFrameTime() * rightPaddleSpeed;
+        right_paddle.y -= GetFrameTime() * right_paddle_speed;
       }
     }
     // Right paddle moving down
     if (IsKeyDown(KEY_DOWN)) {
-      if (rightPaddle.y >= (GetScreenHeight() - rightPaddle.height)) {
-        rightPaddleSpeed += 0;
+      if (right_paddle.y >= (GetScreenHeight() - right_paddle.height)) {
+        right_paddle_speed += 0;
       } else {
-        rightPaddle.y += GetFrameTime() * rightPaddleSpeed;
+        right_paddle.y += GetFrameTime() * right_paddle_speed;
       }
     }
 
-    ball.position.x += GetFrameTime() * ballSpeed.x;
-    ball.position.y += GetFrameTime() * ballSpeed.y;
+    ball.position.x += GetFrameTime() * ball_speed.x;
+    ball.position.y += GetFrameTime() * ball_speed.y;
 
-    ballCollisionWithLeftPaddle =
-        CheckCollisionCircleRec(ball.position, ball.radius, leftPaddle);
-    ballCollisionWithRightPaddle =
-        CheckCollisionCircleRec(ball.position, ball.radius, rightPaddle);
+    ball_collision_with_left_paddle =
+        CheckCollisionCircleRec(ball.position, ball.radius, left_paddle);
+    ball_collision_with_right_paddle =
+        CheckCollisionCircleRec(ball.position, ball.radius, right_paddle);
 
     // Check collisions
     if ((ball.position.x >= (GetScreenWidth() - ball.radius)) ||
         (ball.position.x <= ball.radius)) {
-      ballSpeed.x *= -1.0f;
+      ball_speed.x *= -1.0f;
     }
 
     if (ball.position.y <= ball.radius) {
       ball.position.y = ball.radius;
-      ballSpeed.y *= -1.0f;
+      ball_speed.y *= -1.0f;
     }
     if (ball.position.y >= GetScreenHeight() - ball.radius) {
       ball.position.y = GetScreenHeight() - ball.radius;
-      ballSpeed.y *= -1.0f;
+      ball_speed.y *= -1.0f;
     }
 
-    if (ballCollisionWithLeftPaddle) {
-      ball.position.x = (leftPaddle.x + leftPaddle.width) + ball.radius;
-      if (ballSpeed.x < 0) {
-        ballSpeed.x *= -1.0f;
+    if (ball_collision_with_left_paddle) {
+      ball.position.x = (left_paddle.x + left_paddle.width) + ball.radius;
+      if (ball_speed.x < 0) {
+        ball_speed.x *= -1.0f;
         // increase speed (linear progression)
-        ballSpeed.x = (fabsf(ballSpeed.x) + 25.0f);
+        ball_speed.x = (fabsf(ball_speed.x) + 25.0f);
       }
     }
-    if (ballCollisionWithRightPaddle) {
-      ball.position.x = rightPaddle.x - ball.radius;
-      if (ballSpeed.x > 0) {
-        ballSpeed.x *= -1.0f;
+    if (ball_collision_with_right_paddle) {
+      ball.position.x = right_paddle.x - ball.radius;
+      if (ball_speed.x > 0) {
+        ball_speed.x *= -1.0f;
         // increase speed (linear progression)
-        ballSpeed.x = -(fabsf(ballSpeed.x) + 25.0f);
+        ball_speed.x = -(fabsf(ball_speed.x) + 25.0f);
       }
     }
 
@@ -116,13 +116,13 @@ int main(void) {
     ClearBackground(BLACK);
 
     // Left paddle
-    DrawRectangle(leftPaddle.x, leftPaddle.y, leftPaddle.width,
-                  leftPaddle.height, WHITE);
+    DrawRectangle(left_paddle.x, left_paddle.y, left_paddle.width,
+                  left_paddle.height, WHITE);
     // Ball
     DrawCircleV(ball.position, ball.radius, WHITE);
     // Right paddle
-    DrawRectangle(rightPaddle.x, rightPaddle.y, rightPaddle.width,
-                  rightPaddle.height, WHITE);
+    DrawRectangle(right_paddle.x, right_paddle.y, right_paddle.width,
+                  right_paddle.height, WHITE);
 
     EndDrawing();
   }
