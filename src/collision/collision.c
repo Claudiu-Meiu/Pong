968e183572e1acf6d90e067ef40e41d810234306
Claudiu <claudiu-meiu@proton.me>
2026-08-26 16:12:57 +0300

    Fix ball collision with wall bug: - Pass ball_position as pointer.

--- src/collision/collision.c
+++ src/collision/collision.c
@@ -1,36 +1,37 @@
 #include <math.h>
 #include <raylib.h>
 
-void check_ball_collision_with_wall(Vector2 ball_position, float ball_radius,
+void check_ball_collision_with_wall(Vector2 *ball_position, float ball_radius,
                                     Vector2 *ball_speed) {
   // Top wall
-  if (ball_position.y <= ball_radius) {
-    ball_position.y = ball_radius;
+  if (ball_position->y <= ball_radius) {
+    ball_position->y = ball_radius;
     ball_speed->y *= -1.0f;
   }
   // Bottom wall
-  if (ball_position.y >= GetScreenHeight() - ball_radius) {
-    ball_position.y = GetScreenHeight() - ball_radius;
+  if (ball_position->y >= GetScreenHeight() - ball_radius) {
+    ball_position->y = GetScreenHeight() - ball_radius;
     ball_speed->y *= -1.0f;
   }
 }
 
-void check_ball_collision_with_paddle(Rectangle paddle, Vector2 ball_position,
+void check_ball_collision_with_paddle(Rectangle paddle, Vector2 *ball_position,
                                       float ball_radius, Vector2 *ball_speed) {
+
   bool ball_collision_with_paddle =
-      CheckCollisionCircleRec(ball_position, ball_radius, paddle);
+      CheckCollisionCircleRec(*ball_position, ball_radius, paddle);
 
   if (!ball_collision_with_paddle)
     return;
 
   // Check ball hit vertically on the paddle
   float paddle_center_y = paddle.y + paddle.height / 2.0f;
-  float hit_offset = ball_position.y - paddle_center_y;
+  float hit_offset = ball_position->y - paddle_center_y;
   float hit_position = hit_offset / (paddle.height / 2.0f);
 
   // For the left paddle
   if (paddle.x < GetScreenWidth() / 2.0f) {
-    ball_position.x = paddle.x + paddle.width + ball_radius;
+    ball_position->x = paddle.x + paddle.width + ball_radius;
 
     if (ball_speed->x < 0) {
       ball_speed->x *= -1.0f;
@@ -40,7 +41,7 @@ void check_ball_collision_with_paddle(Rectangle paddle, Vector2 ball_position,
   }
   // For the right paddle
   if (paddle.x > GetScreenWidth() / 2.0f) {
-    ball_position.x = paddle.x - ball_radius;
+    ball_position->x = paddle.x - ball_radius;
 
     if (ball_speed->x > 0) {
       ball_speed->x *= -1.0f;
--- src/pong.c
+++ src/pong.c
@@ -36,14 +36,14 @@ int main(void) {
       check_score_and_reset();
 
       // Check collisions
-      check_ball_collision_with_wall(ball.position, ball.radius,
+      check_ball_collision_with_wall(&ball.position, ball.radius,
                                      &current_ball_speed);
 
-      check_ball_collision_with_paddle(left_paddle, ball.position, ball.radius,
+      check_ball_collision_with_paddle(left_paddle, &ball.position, ball.radius,
                                        &current_ball_speed);
 
-      check_ball_collision_with_paddle(right_paddle, ball.position, ball.radius,
-                                       &current_ball_speed);
+      check_ball_collision_with_paddle(right_paddle, &ball.position,
+                                       ball.radius, &current_ball_speed);
 
     } break;
     default:
