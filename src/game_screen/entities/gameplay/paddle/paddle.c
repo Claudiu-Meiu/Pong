74ea71e28d534a76c81c653b3bff452e88e99214
Claudiu <claudiu-meiu@proton.me>
2026-08-29 04:25:11 +0300

    Clean up paddle module: - Include paddle.h in paddle.c. - Call GetFrameTime() once in dt variable and reuse it for calculations.

--- src/game_screen/entities/gameplay/paddle/paddle.c
+++ src/game_screen/entities/gameplay/paddle/paddle.c
@@ -1,4 +1,4 @@
-#include <raylib.h>
+#include "paddle.h"
 
 Rectangle left_paddle;
 Rectangle right_paddle;
@@ -6,39 +6,41 @@ Rectangle right_paddle;
 float left_paddle_speed = 1500.0f;
 float right_paddle_speed = 1500.0f;
 
-void init_paddle() {
+void init_paddle(void) {
   left_paddle =
       (Rectangle){100.0f - 10.0f / 2.0f,
                   GetScreenHeight() / 2.0f - 100.0f / 2.0f, 20.0f, 100.0f};
   right_paddle =
       (Rectangle){GetScreenWidth() - 100.0f - 10.0f / 2.0f,
                   GetScreenHeight() / 2.0f - 100.0f / 2.0f, 20.0f, 100.0f};
-};
+}
 
-void update_paddle_position() {
+void update_paddle_position(void) {
+  float dt = GetFrameTime();
+
   // Left paddle moving up
   if (IsKeyDown(KEY_W)) {
-    left_paddle.y -= GetFrameTime() * left_paddle_speed;
+    left_paddle.y -= dt * left_paddle_speed;
   }
   // Left paddle moving down
   if (IsKeyDown(KEY_S)) {
-    left_paddle.y += GetFrameTime() * left_paddle_speed;
+    left_paddle.y += dt * left_paddle_speed;
   }
 
   // Right paddle moving up
   if (IsKeyDown(KEY_UP)) {
-    right_paddle.y -= GetFrameTime() * right_paddle_speed;
+    right_paddle.y -= dt * right_paddle_speed;
   }
   // Right paddle moving down
   if (IsKeyDown(KEY_DOWN)) {
-    right_paddle.y += GetFrameTime() * right_paddle_speed;
+    right_paddle.y += dt * right_paddle_speed;
   }
 }
 
-void draw_paddle() {
+void draw_paddle(void) {
   DrawRectangle(left_paddle.x, left_paddle.y, left_paddle.width,
                 left_paddle.height, WHITE);
 
   DrawRectangle(right_paddle.x, right_paddle.y, right_paddle.width,
                 right_paddle.height, WHITE);
-};
+}
--- src/game_screen/entities/gameplay/paddle/paddle.h
+++ src/game_screen/entities/gameplay/paddle/paddle.h
@@ -1,5 +1,5 @@
-#ifndef _PADDLE_H
-#define _PADDLE_H
+#ifndef PADDLE_H
+#define PADDLE_H
 
 #include <raylib.h>
 
@@ -9,8 +9,8 @@ extern Rectangle right_paddle;
 extern float left_paddle_speed;
 extern float right_paddle_speed;
 
-void init_paddle();
-void update_paddle_position();
-void draw_paddle();
+void init_paddle(void);
+void update_paddle_position(void);
+void draw_paddle(void);
 
 #endif
