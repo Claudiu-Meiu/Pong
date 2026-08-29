8000d32aed6dd160cd96f18b927d87b42b4fdc9d
Claudiu <claudiu-meiu@proton.me>
2026-08-29 05:01:31 +0300

    Clean up sound effect module: - Move raylib include in header file.

--- src/sound_effect/sound_effect.c
+++ src/sound_effect/sound_effect.c
@@ -1,21 +1,21 @@
-#include <raylib.h>
+#include "sound_effect.h"
 
 Sound paddle_hit_sound;
 Sound wall_hit_sound;
 Sound score_point_sound;
 Sound endgame_sound;
 
-Sound init_paddle_hit_sound() {
+Sound init_paddle_hit_sound(void) {
   return paddle_hit_sound = LoadSound("resources/sounds/paddle_hit.wav");
 }
-Sound init_wall_hit_sound() {
+Sound init_wall_hit_sound(void) {
   return wall_hit_sound = LoadSound("resources/sounds/wall_hit.wav");
 }
 
-Sound init_score_point_sound() {
+Sound init_score_point_sound(void) {
   return score_point_sound = LoadSound("resources/sounds/score_point.wav");
 }
 
-Sound init_endgame_sound() {
+Sound init_endgame_sound(void) {
   return endgame_sound = LoadSound("resources/sounds/endgame.wav");
 }
--- src/sound_effect/sound_effect.h
+++ src/sound_effect/sound_effect.h
@@ -1,5 +1,5 @@
-#ifndef _SOUND_EFFECT_H
-#define _SOUND_EFFECT_H
+#ifndef SOUND_EFFECT_H
+#define SOUND_EFFECT_H
 
 #include <raylib.h>
 
@@ -8,9 +8,9 @@ extern Sound paddle_hit_sound;
 extern Sound score_point_sound;
 extern Sound endgame_sound;
 
-Sound init_paddle_hit_sound();
-Sound init_wall_hit_sound();
-Sound init_score_point_sound();
-Sound init_endgame_sound();
+Sound init_paddle_hit_sound(void);
+Sound init_wall_hit_sound(void);
+Sound init_score_point_sound(void);
+Sound init_endgame_sound(void);
 
 #endif
