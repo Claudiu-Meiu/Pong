c140de7646472d99cc57124bed44f5c21af275d1
Claudiu <claudiu-meiu@proton.me>
2026-08-29 04:06:26 +0300

    Clean up dividing line module.

--- src/game_screen/entities/gameplay/dividing_line/dividing_line.c
+++ src/game_screen/entities/gameplay/dividing_line/dividing_line.c
@@ -1,13 +1,13 @@
-#include <raylib.h>
+#include "dividing_line.h"
 
 Vector2 dividing_line_start;
 Vector2 dividing_line_end;
 
-void init_dividing_line() {
+void init_dividing_line(void) {
   dividing_line_start = (Vector2){GetScreenWidth() / 2.0f, 0.0f};
   dividing_line_end = (Vector2){GetScreenWidth() / 2.0f, GetScreenHeight()};
 }
 
-void draw_dividing_line() {
+void draw_dividing_line(void) {
   DrawLineEx(dividing_line_start, dividing_line_end, 2.0f, DARKGRAY);
 }
--- src/game_screen/entities/gameplay/dividing_line/dividing_line.h
+++ src/game_screen/entities/gameplay/dividing_line/dividing_line.h
@@ -1,12 +1,12 @@
-#ifndef _DIVIDING_LINE_H
-#define _DIVIDING_LINE_H
+#ifndef DIVIDING_LINE_H
+#define DIVIDING_LINE_H
 
 #include <raylib.h>
 
 extern Vector2 dividing_line_start;
 extern Vector2 dividing_line_end;
 
-void init_dividing_line();
-void draw_dividing_line();
+void init_dividing_line(void);
+void draw_dividing_line(void);
 
 #endif
